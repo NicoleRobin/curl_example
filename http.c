d1bf62175102af1b67aa0cd0b3cd66abc285892b
nicolerobin <lit050528@gmail.com>
2018-04-24 14:07:38 +0800

    1.implemente http_post

--- http.c
+++ http.c
@@ -21,59 +21,60 @@ size_t write_cb(char *ptr, size_t size, size_t nmemb, void *arg)
 int http_get(const char *pUrl, char *result, int len)
 {
 	// printf("In http_get, url:%s\n", pUrl);
-	if (0 != curl_global_init(CURL_GLOBAL_ALL)) {
-		printf("In http_get, curl_global_init() failed\n");
+	CURLcode ret = CURLE_OK;
+	ret = curl_global_init(CURL_GLOBAL_ALL);
+	if (ret != CURLE_OK) {
+		printf("In http_get, curl_global_init() failed, error:%s\n", 
+				curl_easy_strerror(ret));
 		return -1;
 	}
 
 	CURL *pCurl = curl_easy_init();
-	if (!pCurl) {
+	if (pCurl == NULL) {
 		printf("In http_get, curl_easy_init() failed\n");
 		return -2;
 	}
-	else {
-		CURLcode ret;
-		struct STWriteData writeData;
-		writeData.pData = result;
-		writeData.len = len;
-		ret = curl_easy_setopt(pCurl, CURLOPT_URL, pUrl);
-		if (ret != CURLE_OK) {
-			printf("In http_get, curl_easy_setopt() failed, error:%s\n", curl_easy_strerror(ret));
-			return ret;
-		}
 
-		/*
-		ret = curl_easy_setopt(pCurl, CURLOPT_HTTPGET, 1L);
-		if (ret != CURLE_OK) {
-			printf("In http_get, curl_easy_setopt() failed, error:%s\n", curl_easy_strerror(ret));
-			return ret;
-		}
-		*/
-		ret = curl_easy_setopt(pCurl, CURLOPT_FOLLOWLOCATION, 1L);
-		if (ret != CURLE_OK) {
-			printf("In http_get, curl_easy_setopt() failed, error:%s\n", curl_easy_strerror(ret));
-			return ret;
-		}
-
-		ret = curl_easy_setopt(pCurl, CURLOPT_WRITEFUNCTION, write_cb);
-		if (ret != CURLE_OK) {
-			printf("In http_get, curl_easy_setopt() failed, error:%s\n", curl_easy_strerror(ret));
-			return ret;
-		}
-
-		ret = curl_easy_setopt(pCurl, CURLOPT_WRITEDATA, &writeData); 
-		if (ret != CURLE_OK) {
-			printf("In http_get, curl_easy_setopt() failed, error:%s\n", curl_easy_strerror(ret));
-			return ret;
-		}
-
-		ret = curl_easy_perform(pCurl);
-		if (ret != CURLE_OK) {
-			printf("In http_get, curl_easy_perform() failed, %s\n", curl_easy_strerror(ret));
-			return ret;
-		}
-		curl_easy_cleanup(pCurl);
+	struct STWriteData writeData;
+	writeData.pData = result;
+	writeData.len = len;
+	ret = curl_easy_setopt(pCurl, CURLOPT_URL, pUrl);
+	if (ret != CURLE_OK) {
+		printf("In http_get, curl_easy_setopt() failed, error:%s\n", curl_easy_strerror(ret));
+		return ret;
 	}
+
+	/*
+	ret = curl_easy_setopt(pCurl, CURLOPT_HTTPGET, 1L);
+	if (ret != CURLE_OK) {
+		printf("In http_get, curl_easy_setopt() failed, error:%s\n", curl_easy_strerror(ret));
+		return ret;
+	}
+	*/
+	ret = curl_easy_setopt(pCurl, CURLOPT_FOLLOWLOCATION, 1L);
+	if (ret != CURLE_OK) {
+		printf("In http_get, curl_easy_setopt() failed, error:%s\n", curl_easy_strerror(ret));
+		return ret;
+	}
+
+	ret = curl_easy_setopt(pCurl, CURLOPT_WRITEFUNCTION, write_cb);
+	if (ret != CURLE_OK) {
+		printf("In http_get, curl_easy_setopt() failed, error:%s\n", curl_easy_strerror(ret));
+		return ret;
+	}
+
+	ret = curl_easy_setopt(pCurl, CURLOPT_WRITEDATA, &writeData); 
+	if (ret != CURLE_OK) {
+		printf("In http_get, curl_easy_setopt() failed, error:%s\n", curl_easy_strerror(ret));
+		return ret;
+	}
+
+	ret = curl_easy_perform(pCurl);
+	if (ret != CURLE_OK) {
+		printf("In http_get, curl_easy_perform() failed, %s\n", curl_easy_strerror(ret));
+		return ret;
+	}
+	curl_easy_cleanup(pCurl);
 
 	curl_global_cleanup();
 	return 0;
@@ -81,5 +82,57 @@ int http_get(const char *pUrl, char *result, int len)
 
 int http_post(const char *pUrl, const char *body, char *result, int len)
 {
+	printf("In http_post(), pUrl:%s\n", pUrl);
+	printf("In http_post(), body:%s\n", body);
+	CURL *pCurl = NULL;
+	CURLcode ret = CURLE_OK;
+	ret = curl_global_init(CURL_GLOBAL_ALL);
+	if (ret != 0) {
+		printf("In http_post, curl_global_init() failed, error:%s\n", 
+				curl_easy_strerror(ret));
+		return -1;
+	}
+
+	pCurl = curl_easy_init();
+	if (pCurl == NULL) {
+		printf("In http_post, curl_easy_init() failed, error:%s\n", 
+				curl_easy_strerror(ret));
+		return -2;
+	}
+
+	ret = curl_easy_setopt(pCurl, CURLOPT_URL, pUrl); 
+	if (ret != CURLE_OK) {
+		printf("In http_post, curl_easy_setopt() failed, error:%s\n", 
+				curl_easy_strerror(ret));
+		return ret;
+	}
+
+	struct curl_slist *pList = NULL;
+	pList = curl_slist_append(pList, 
+			"Content-Type:application/json;");
+	ret = curl_easy_setopt(pCurl, CURLOPT_HTTPHEADER, pList);
+	if (ret != CURLE_OK) {
+		printf("In http_post, curl_easy_setopt() failed, error:%s\n", 
+				curl_easy_strerror(ret));
+		return ret;
+	}
+
+	ret = curl_easy_setopt(pCurl, CURLOPT_POSTFIELDS, body);
+	if (ret != CURLE_OK) {
+		printf("In http_post, curl_easy_setopt() failed, error:%s\n", 
+				curl_easy_strerror(ret));
+		return ret;
+	}
+
+	ret = curl_easy_perform(pCurl);
+	if (ret != CURLE_OK) {
+		printf("In http_post, curl_easy_perform() failed, error:%s\n", 
+				curl_easy_strerror(ret));
+		return ret;
+	}
+
+	curl_slist_free_all(pList);
+	curl_easy_cleanup(pCurl);
+	curl_global_cleanup();
 	return 0;
 }
